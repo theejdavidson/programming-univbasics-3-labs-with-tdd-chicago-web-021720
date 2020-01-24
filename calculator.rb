7ea3574b29e60377879410bd6b20e7e17d458aca
Ethan Davidson <theejdavidson@gmail.com>
2020-01-24 17:41:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ first_number = 1.5
 second_number = 1.5
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotent = first_number / second_number
